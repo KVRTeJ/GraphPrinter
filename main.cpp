acf27ae3fd3311c24169aae8ed7b28a4da1eed07
KVRTeJ <bb.sssss@inbox.ru>
2025-05-02 13:49:30 +0700

    Переименован проект на GraphPrinter. Создан новый проект для 2-ой части лабораторной работы

--- main.cpp
+++ main.cpp
@@ -1,51 +1,11 @@
-#include <iostream>
+#include "mainwindow.h"
 
-#include "Computer.h"
+#include <QApplication>
 
-template <typename ProcType>
-void makeFunctor(IOCContainer& ioc) {
-    ioc.RegisterFunctor<Processor, std::string, Processor::ProcessorType, double>(
-        std::function<std::shared_ptr<Processor>(std::shared_ptr<std::string>,
-                                                 std::shared_ptr<Processor::ProcessorType>,
-                                                 std::shared_ptr<double>)>([] (std::shared_ptr<std::string> version,
-                                                                              std::shared_ptr<Processor::ProcessorType> type,
-                                                                              std::shared_ptr<double> speed)->std::shared_ptr<Processor>
-                                                                           {
-                                                                               return std::make_shared<ProcType>(*version, *type, *speed);
-                                                                           }));
-}
-
-int main() {
-    IOCContainer ioc;
-
-    std::cout << "\n\tПример" << std::endl;
-    ioc.RegisterInstance<Processor>(std::make_shared<IntelProcessor>("version 3.1", Processor::ProcessorType::x86, 4.555));
-    Computer comp(ioc.GetObject<Processor>());
-    comp.configure();
-
-    ioc.RegisterInstance<Processor>(std::make_shared<AmdProcessor>("version 3.1", Processor::ProcessorType::x86, 3.111));
-    comp.setProcessor(ioc);
-    comp.configure();
-
-    std::cout << "\n\tПример" << std::endl;
-    ioc.RegisterInstance<std::string>(std::make_shared<std::string>("version 2"));
-    ioc.RegisterInstance<Processor::ProcessorType>(std::make_shared<Processor::ProcessorType>(Processor::x64));
-    ioc.RegisterInstance<double>(std::make_shared<double>(3.111));
-
-    makeFunctor<IntelProcessor>(ioc);
-
-    ioc.RegisterFactory<Computer, Computer, Processor>();
-    auto boo = ioc.GetObject<Computer>();
-    boo->configure();
-
-    makeFunctor<AmdProcessor>(ioc);
-
-    boo->setProcessor(ioc);
-    boo->configure();
-
-    std::cout << "\n\tПример" << std::endl;
-    ioc.RegisterFactory<Processor, IntelProcessor, std::string, Processor::ProcessorType, double>();
-    Computer comp3 = ioc.GetObject<Processor>();
-    comp3.configure();
-    return 0;
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
