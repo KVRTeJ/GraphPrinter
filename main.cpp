3d892ec8d201f03654c048673af02ffbc1c66ed4
KVRTeJ <bb.sssss@inbox.ru>
2025-05-18 22:40:57 +0700

    Убран код из main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
-#include "mainwindow.h"
+#include <iostream>
 
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+int main(int argc, char *argv[]) {
+    std::cout << "Here will be parser. . . " << std::endl;
 }
