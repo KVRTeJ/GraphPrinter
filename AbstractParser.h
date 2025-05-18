74335303a4880337befbc8b4c5645ed37113824c
KVRTeJ <bb.sssss@inbox.ru>
2025-05-19 00:12:47 +0700

    feat: добавлена реализация конструктора, _data теперь protected, нужно чтобы наследники имели доступ к данным)

--- AbstractParser.h
+++ AbstractParser.h
@@ -3,7 +3,7 @@
 
 #include <qvector.h>
 
-#include "IParser"
+#include "IParser.h"
 
 struct Data {
     double xAxis = 0;
@@ -12,7 +12,20 @@ struct Data {
 
 class AbstractParser : public IParser<QVector<Data>> {
 public:
+    AbstractParser(const QString& filePath = "") : _filePath(filePath)
+    {}
+    ~AbstractParser() override = default;
 
-}
+    QString getFilePath() const override {return _filePath;}
+    void setFilePath(const QString& filePath) override {_filePath = filePath;}
+
+    QVector<Data> getData() const override {return _data;}
+
+protected:
+    QVector<Data> _data {};
+
+private:
+    QString _filePath {""};
+};
 
 #endif // ABSTRACTPARSER_H
