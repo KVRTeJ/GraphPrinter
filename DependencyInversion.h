8f6a3f2dc493532572361d73d30423a8d10c39aa
KVRTeJ <bb.sssss@inbox.ru>
2025-05-29 06:40:42 +0700

    feat: Создал класс который отвечает за инверсию зависимости

---
+++ DependencyInversion.h
@@ -0,0 +1,50 @@
+#ifndef DEPENDENCYINVERSION_H
+#define DEPENDENCYINVERSION_H
+
+
+#include "IOC_Contaner.h"
+
+#include "IParser.h"
+#include "JsonParser.h"
+#include "SqlLiteParser.h"
+
+#include "IChartCreator.h"
+#include "LineChartCreator.h"
+#include "AreaChartCreator.h"
+#include "SplineChartCreator.h"
+#include "ScatterChartCreator.h"
+
+#include "DataExtract.h"
+
+#include <QObject>
+#include <QtCore/qdebug.h>
+
+static IOCContainer gContainer;
+
+class DIConfiguration {
+public:
+    static void configure() {
+        gContainer.RegisterInstance<DataModel, DataModel>();
+
+        gContainer.RegisterInstance<IParser, JsonParser>();
+
+        gContainer.RegisterInstance<IChartCreator, LineChartCreator>();
+    }
+
+    static void switchToJsonParser(IDataExtracter* extracter) {
+        gContainer.RegisterInstance<IParser>(std::make_shared<JsonParser>("", extracter));
+    }
+    static void switchToSqlLiteParser(IDataExtracter* extracter) {
+        gContainer.RegisterInstance<IParser>(std::make_shared<SqlLiteParser>("", extracter));
+    }
+
+    static void switchToLineChart() {
+        gContainer.RegisterInstance<IChartCreator, LineChartCreator>();
+    }
+    static void switchToAreaChart() {
+        gContainer.RegisterInstance<IChartCreator, AreaChartCreator>();
+    }
+
+};
+
+#endif // DEPENDENCYINVERSION_H
