5a6f4de88905f66f7830182401cdded6492f1dc9
KVRTeJ <bb.sssss@inbox.ru>
2025-05-29 03:30:47 +0700

    test: протестировал приложение с графиком

--- main.cpp
+++ main.cpp
@@ -4,10 +4,33 @@
 
 #include <QDebug>
 
+#include "SqlLiteParser.h"
+#include "DataExtract.h"
+
+#include "LineChartCreator.h"
+#include "AreaChartCreator.h"
+#include "ScatterChartCreator.h"
+#include "SplineChartCreator.h"
+
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
+    SqlLiteParser par;
+    GraphDataExtracter ext;
+    par.setDataExtracter(&ext);
+    par.setFilePath("/Users/dmitriy/data/BLOOD_SUGAR.sqlite");
+    par.parse();
+
+    DataModel model;
+    model.setData(par.getData());
+    auto data = model.getData();
+    LineChartCreator lineChartCreator;
+
+    auto line = lineChartCreator.create(data);
+
     ApplicationWindow w;
+
+    w.displayChart(line);
     w.show();
 
     return a.exec();
